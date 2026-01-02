0add26f44e90071ee9eed3601ad650f17e196632
doolingdavidrs21 <david@rs21.io>
2026-01-02 14:43:58 -0700

    updated code

---
+++ code/Chapter 19/vscode_playground.py
@@ -0,0 +1,9 @@
+# https://python.land/creating-python-programs/python-in-vscode
+import sys
+
+if len(sys.argv) == 2:
+    name = sys.argv[1]
+else:
+    name = 'stranger'
+
+print(f'Hi there, {name}')
