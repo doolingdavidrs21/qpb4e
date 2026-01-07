048d2c43319f0fc0939a1e560473b6efc645bfe7
doolingdavidrs21 <david@rs21.io>
2026-01-07 15:07:40 -0700

    Update coroutine.py to demonstrate multiple async tasks
    Add id parameter to fetch_data() and create two concurrent tasks
    to better illustrate asyncio coroutine behavior.

--- code/Chapter 19/coroutine.py
+++ code/Chapter 19/coroutine.py
@@ -2,21 +2,26 @@ import asyncio
 
 
 # define a coroutine that simulates a time-consuming task
-async def fetch_data(delay):
-    print("Fetching data...")
+async def fetch_data(delay, id):
+    print("Fetching data... id:", id)
     await asyncio.sleep(delay) # simulate an I/O operation with a sleep
-    print("Data fetched")
-    return {"data": "some data"}
+    print("Data fetched, id:", id)
+    return {"data": "some data", "id": id}
 
 # dfeine another coroutine that calls the first coroutine
 
 async def main():
     print("Start of main coroutine")
-    task = fetch_data(2)
+    task1 = fetch_data(2, 1)
+    task2 = fetch_data(2, 2)
     # Await the fetch_data coroutine, pausing execution of main until
     # fetch_data completes
-    result = await task
-    print(f"Received result: {result}")
+    result1 = await task1
+    print(f"Received result: {result1}")
+    result2 = await task2
+    print(f"Received result: {result2}")
+
+
     print("End of main coroutine")
 
 # Run the main coroutine
