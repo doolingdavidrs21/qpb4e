52135d22e40a6f7afb8dcbdb889811dd546b1b1f
doolingdavidrs21 <david@rs21.io>
2026-01-14 10:05:04 -0700

    updated code

--- code/Chapter 19/arguments.sh
+++ code/Chapter 19/arguments.sh
@@ -1,3 +1,21 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -u
 echo "Hello $1, from $0"
+
+echo "You can list numbers and text like this:"
+
+for n in 1 2 3 four
+do
+   echo "Number $n"
+done
+
+for n in {1..5}
+do
+    echo "Number $n"
+done
+
+echo "Or use the output of another command:"
+for f in $(ls)
+do
+    echo $f
+done
