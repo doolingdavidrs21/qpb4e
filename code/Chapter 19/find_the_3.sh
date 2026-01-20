a615fe8f5077cf265c62150619e1ad5e098273f1
doolingdavidrs21 <david@rs21.io>
2026-01-20 10:21:34 -0700

    updated code

---
+++ code/Chapter 19/find_the_3.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+for i in {1..10}
+do
+    if test $i -eq 3
+    then
+        echo "I found the 3!"
+    else
+        echo "Not looking for the $i"
+    fi
+done
---
+++ code/Chapter 19/newarguments.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+if test -z "$1"
+then
+    echo "Usage: $0 <Your name>"
+else
+    echo "Hello $1, from $0"
+fi
+
